703750a83d1590ffde50efcfe45cc3a5f8e5890b
Bence Monus <crome@moronia.hu>
2018-05-14 23:31:50 +0100

    delete empty constructor

--- Mixer.cs
+++ Mixer.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 
 namespace chirpcore {
     public class Mixer {
-        public Mixer() {
-
-        }
-
         public void Mix(double[] output, List<double[]> buffers) {
+            if (buffers.Count == 0) { return; }
+
             for (int i=0; i<output.Length; i++) {
                 double value = buffers[0][i];
                 for (int j=1; j<buffers.Count; j++) {
