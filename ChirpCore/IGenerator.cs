d4d51f32985af29b99887f298d904190154a36a1
Bence Monus <crome@moronia.hu>
2018-05-20 23:34:20 +0100

    remove .net core project

--- ChirpCore/IGenerator.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace chirpcore {
-    public interface IGenerator {
-        void Fill(double[] buffer, double frequency, int frames);
-    }
-}
--- ChirpCore/TriggerMode.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace chirpcore {
-    public enum TriggerMode : int {
-        Continuous = 0,
-        Pluck = 1,
-    }
-}
