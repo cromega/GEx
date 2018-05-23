acd62c18084c2f9c10ed79af16ab5361a23d6a83
Bence Monus <crome@moronia.hu>
2018-05-23 22:35:03 +0100

    log this until I fix this shit

--- Chirpesizer/Mixer.cs
+++ Chirpesizer/Mixer.cs
@@ -10,6 +10,9 @@ namespace Chirpesizer {
                 double value = buffers[0][i];
                 for (int j=1; j<buffers.Count; j++) {
                     value += buffers[j][i];
+                    if (value > short.MaxValue) {
+                        Logger.Log("clipping {0}", value);
+                    }
                 }
 
                 output[i] = Math.Min(value, short.MaxValue);
