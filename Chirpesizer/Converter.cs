7ed72d60351f45eee60a837ed26ba9330b004838
Bence Monus <crome@moronia.hu>
2018-05-24 14:32:24 +0100

    cleanup

--- Chirpesizer/Converter.cs
+++ Chirpesizer/Converter.cs
@@ -6,19 +6,6 @@ namespace Chirpesizer {
             for (int i=0; i<source.Length; i++) {
                 output[i] = (short)source[i];
             }
-            return;
-
-            var minmax = source[0];
-
-            for (int i=0; i<source.Length; i++) {
-                if (Math.Abs(source[i]) > minmax) { minmax = source[i]; }
-            }
-
-            var ratio = short.MaxValue / minmax;
-            // Logger.Log("normalized buffer to {0}", ratio);
-            for (int i=0; i<source.Length; i++) {
-                output[i] = (short)(source[i] * ratio);
-            }
         }
     }
 }
