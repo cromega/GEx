4890a77cb4e38a0bfc096ecc883bf30d57a80dcf
Bence Monus <crome@moronia.hu>
2018-06-06 17:35:19 +0100

    add more tracker bits

--- Chirpotle/Tracker/tracker.js
+++ Chirpotle/Tracker/tracker.js
@@ -1,5 +1,10 @@
 ﻿$(document).ready(function () {
 	loadTracker($("#tracker"));
+	$("body").on("keypress", function(evt) {
+		if ($(".selected").length == 0) { return; }
+		var label = $(".selected.note_label").first();
+		label.text(evt.key);
+	});
 });
 
 var loadTracker = function (tracker) {
@@ -8,11 +13,24 @@ var loadTracker = function (tracker) {
 	}
 };
 
-var appendTrack = function (tracker) {
-	$(tracker).append("<div class=\"track_line\"></div>");
-	var line = $(".track_line").last();
-	for (var i = 0; i < 32; i++) {
-		line.append("<div class=\"node\"></div>");
+var appendTrack = function(tracker) {
+	var column = $(document.createElement("div")).addClass("track_line").appendTo(tracker);
+	$(document.createElement("div")).addClass("label").on("dblclick", function(evt) {
+		var menu = $(document.createElement("select")).css({top: evt.clientY, left: evt.clientX, position: "absolute"}).on("change", function(evt) {
+			$("select option:selected").each(function() {
+				console.log(this.text());
+				menu.remove();
+			})
+		}).appendTo($("body"));
+	}).appendTo(column);
+
+	for (var i=0; i < 32; i++) {
+		var node = $(document.createElement("div")).addClass("node").on("click", function(evt) {
+			$(".selected").removeClass("selected");
+			$(evt.target).addClass("selected");
+		}).appendTo(column);
+		$(document.createElement("div")).addClass("trigger").appendTo(node);
+		$(document.createElement("div")).addClass("note_label").appendTo(node);
 	}
-	line.append("<div class=\"clear\"></div>");
+	$(document.createElement("div")).addClass("clear").appendTo(column);
 }
