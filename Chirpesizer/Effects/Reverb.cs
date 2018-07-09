75f534759fad4bd07868567f1fe34e52c5cffe83
Bence Monus <crome@moronia.hu>
2018-07-09 21:18:17 +0100

    make reverb use both channels

--- Chirpesizer/Effects/Reverb.cs
+++ Chirpesizer/Effects/Reverb.cs
@@ -3,44 +3,50 @@
 namespace Chirpesizer.Effects {
     public class Reverb : IEffect {
         public readonly int Delay;
-        public readonly double Dampen;
+        public readonly double Decay;
 
         private double[] DelayBuffer;
 
-        public Reverb(int delay, double dampen) {
+        public Reverb(int delay, double decay) {
             Delay = delay;
-            Dampen = dampen;
-            DelayBuffer = new double[delay];
+            Decay = decay;
+            DelayBuffer = new double[delay * 2];
         }
 
         public double[] Apply(double[] input) {
             var output = (double[])input.Clone();
 
-            if (input.Length > Delay) {
+            if (input.Length/2 > Delay) {
                 // apply carried over delay buffer
                 for (int i=0; i < Delay; i++) {
-                    output[i] += DelayBuffer[i];
+                    output[i*2] += DelayBuffer[i*2];
+                    output[i*2+1] += DelayBuffer[i*2+1];
                 }
 
                 // apply delay within input
-                for (int i = 0; i < input.Length - Delay; i++) {
-                    output[i + Delay] += output[i] * Dampen;
+                for (int i = 0; i < input.Length/2 - Delay; i++) {
+                    output[(i + Delay)*2] += output[i*2] * Decay;
+                    output[(i + Delay)*2+1] += output[i*2+1] * Decay;
                 }
 
-                // update the delay buffer
+                // copy the end of the input into the delay buffer
                 for (int i=0; i<Delay; i++) {
-                    DelayBuffer[i] = input[input.Length - Delay + i] * Dampen;
+                    DelayBuffer[i*2] = input[input.Length/2 - Delay + (i*2)] * Decay;
+                    DelayBuffer[i*2+1] = input[input.Length/2 - Delay + (i*2+1)] * Decay;
                 }
             } else { // if (Delay >= input.Length)
                 // apply carried over delay buffer
-                for (int i=0; i < input.Length; i++) {
-                    output[i] += DelayBuffer[i];
+                for (int i=0; i < input.Length/2; i++) {
+                    output[i*2] += DelayBuffer[i*2];
+                    output[i*2+1] += DelayBuffer[i*2+1];
                 }
 
-                // update the delay buffer
-                Array.Copy(DelayBuffer, input.Length, DelayBuffer, 0, Delay - input.Length);
-                for (int i=0; i<input.Length; i++) {
-                    DelayBuffer[Delay - output.Length + i] = output[i] * Dampen;
+                // shift the unused part of the delay buffer to the front
+                // and update the end with the input
+                Array.Copy(DelayBuffer, input.Length, DelayBuffer, 0, Delay*2 - input.Length);
+                for (int i=0; i<input.Length/2; i++) {
+                    DelayBuffer[Delay - output.Length/2 + (i*2)] = output[i*2] * Decay;
+                    DelayBuffer[Delay - output.Length/2 + (i*2+1)] = output[i*2+1] * Decay;
                 }
             }
             return output;
--- Test/TestReverb.cs
+++ Test/TestReverb.cs
@@ -6,48 +6,25 @@ using FluentAssertions;
 namespace Test {
     public class TestReverb {
         [Fact]
-        public void TestEchoEffect() {
-            var data = new double[] { 10, 20, 30, 10, 20, 30, 10, 20, 30 };
-            var reverb = new Reverb(5, 0.5);
-
-            var chunk = reverb.Apply(data.Take(3).ToArray());
-            chunk.Should().Equal(10, 20, 30);
-
-            chunk = reverb.Apply(data.Skip(3).Take(3).ToArray());
-            chunk.Should().Equal(10, 20, 35);
-
-            chunk = reverb.Apply(data.Skip(6).Take(3).ToArray());
-            chunk.Should().Equal(20, 35, 35);
-        }
-
-        [Fact]
-        public void TestReverbWithBufferLongerThanDelay() {
-            var data = new double[] { 10, 20, 30, 10, 20, 30, 10, 20, 30 };
-            var reverb = new Reverb(5, 0.5);
-            var output = reverb.Apply(data);
-            output.Should().Equal(10, 20, 30, 10, 20, 35, 20, 35, 35);
-        }
-
-        [Fact]
         public void TestFeedback() {
-            var data = new double[] { 20, 0, 0, 0, 0, 0 };
+            var data = new double[] { 20, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
             var reverb = new Reverb(2, 0.5);
             var output = reverb.Apply(data);
-            output.Should().Equal(20, 0, 10, 0, 5, 0);
+            output.Should().Equal(20, 20, 0, 0, 10, 10, 0, 0, 5, 5, 0, 0);
         }
 
         [Fact]
         public void TestFeedbackWithMultipleChunks() {
-            var data = new double[] { 20, 0, 0, 0, 0, 0 };
+            var data = new double[] { 20, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
             var reverb = new Reverb(2, 0.5);
-            var chunk = reverb.Apply(data.Take(2).ToArray());
-            chunk.Should().Equal(20, 0);
+            var chunk = reverb.Apply(data.Take(4).ToArray());
+            chunk.Should().Equal(20, 20, 0, 0);
 
-            chunk = reverb.Apply(data.Skip(2).Take(2).ToArray());
-            chunk.Should().Equal(10, 0);
+            chunk = reverb.Apply(data.Skip(4).Take(4).ToArray());
+            chunk.Should().Equal(10, 10, 0, 0);
 
-            chunk = reverb.Apply(data.Skip(4).Take(2).ToArray());
-            chunk.Should().Equal(5, 0);
+            chunk = reverb.Apply(data.Skip(8).Take(4).ToArray());
+            chunk.Should().Equal(5, 5, 0, 0);
         }
 
     }
