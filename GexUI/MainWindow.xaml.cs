8a17c2c879e3c8444d58084d8173275d956504c5
Bence Monus <crome@moronia.hu>
2018-07-18 00:16:18 +0100

    add project for UI v2

---
+++ GexUI/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GraphExperiment;
+
+namespace GexUI {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            NodeList.MouseDoubleClick += InstrumentsList_MouseDoubleClick;
+            PatchEditor.LayoutTransform = new ScaleTransform();
+            PatchEditor.MouseWheel += PatchEditor_MouseWheel;
+            PatchEditor.MouseLeftButtonDown += PatchEditor_MouseLeftButtonDown;
+        }
+
+        private void PatchEditor_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            (sender as Canvas).Focus();
+        }
+
+        private void PatchEditor_MouseWheel(object sender, MouseWheelEventArgs e) {
+            var st = PatchEditor.LayoutTransform as ScaleTransform;
+
+            if (e.Delta > 0) {
+                st.ScaleX *= 1.1;
+                st.ScaleY *= 1.1;
+            } else {
+                st.ScaleX /= 1.1;
+                st.ScaleY /= 1.1;
+            }
+        }
+
+        private void InstrumentsList_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            var list = (ListBox)sender;
+            var node = new AudioNode((string)list.SelectedItem);
+            node.Margin = new Thickness(100, 100, 0, 0);
+            MainContainer.Children.Add(node);
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e) {
+            foreach (string name in GetAudioControls()) {
+               NodeList.Items.Add(name);
+            }
+        }
+
+        private IEnumerable<string> GetAudioControls() {
+            foreach (var an in Assembly.GetExecutingAssembly().GetReferencedAssemblies()) {
+                var asm = Assembly.Load(an);
+                foreach (var type in asm.GetTypes()) {
+                    if (type.GetCustomAttributes(typeof(AudioNodeAttribute), inherit: false).Length > 0) {
+                        yield return type.Name;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GraphExperiment/ControlTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphExperiment {
+    public class AudioNodeAttribute : Attribute {}
+    public class AudioNodeParameterAttribute : Attribute {}
+
+    [AudioNode]
+    public class ControlTest {
+        [AudioNodeParameter]
+        public double Frequency;
+
+        [AudioNodeParameter]
+        public int Count;
+    }
+}
