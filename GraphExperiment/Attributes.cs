97ad96113723a0f0db77d7e27518e66055387735
Bence Monus <crome@moronia.hu>
2018-07-19 20:00:39 +0100

    move attributes into synth

---
+++ GraphExperiment/Attributes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphExperiment {
+    public enum AudioNodeDirection {
+        OutputOnly,
+        InputOutput,
+    }
+
+    public class AudioNodeAttribute : Attribute {
+        public AudioNodeDirection Direction;
+    }
+
+    public class AudioNodeParameterAttribute : Attribute {}
+}
--- GraphExperiment/ControlTest.cs
+++ GraphExperiment/ControlTest.cs
@@ -5,16 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace GraphExperiment {
-    public enum AudioNodeDirection {
-        OutputOnly,
-        InputOutput,
-    }
-
-    public class AudioNodeAttribute : Attribute {
-        public AudioNodeDirection Direction;
-    }
-    public class AudioNodeParameterAttribute : Attribute {}
-
     [AudioNode(Direction = AudioNodeDirection.InputOutput)]
     public class ControlTest {
         [AudioNodeParameter]
