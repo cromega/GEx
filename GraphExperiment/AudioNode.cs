1d087320692a568e94ab157936084c3a8b952690
Bence Monus <crome@moronia.hu>
2018-07-23 00:47:43 +0100

    add memory for storing node state

--- GraphExperiment/AudioNode.cs
+++ GraphExperiment/AudioNode.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace GraphExperiment {
     public abstract class AudioNode {
@@ -10,9 +11,12 @@ namespace GraphExperiment {
         public readonly int Id;
         public Wire Connection;
         public Wire Input;
+        private Dictionary<string, Hashtable> Memory;
+        private Hashtable State;
 
         public AudioNode(int id) {
-
+            Input = new Wire(4410);
+            Memory = new Dictionary<string, Hashtable>();
         }
 
         public AudioNode(int id, Wire connection) {
@@ -27,7 +31,27 @@ namespace GraphExperiment {
 
         public Packet Read() {
             var packet = Input.Take();
-            return Input.Take();
+            if (!Memory.ContainsKey(packet.TriggerID)) {
+                Memory[packet.TriggerID] = new Hashtable();
+            }
+            State = Memory[packet.TriggerID];
+            return packet;
+
+            //Send(Update(packet));
+        }
+
+        //protected abstract Packet Update(Packet packet);
+
+        protected T Fetch<T>(string key) {
+            return (T)(State[key]);
+        }
+
+        protected void Save(string key, object value) {
+            State[key] = value;
+        }
+
+        protected bool IsSaved(string key) {
+            return State.ContainsKey(key);
         }
     }
 }
