e32dce746a116374257fc7d7beffe3053e165b0b
Bence Monus <crome@moronia.hu>
2018-08-16 15:16:53 +0100

    use a phase accumulation oscillator for variable freq

--- GraphExperiment/AudioNodes/Generator.cs
+++ GraphExperiment/AudioNodes/Generator.cs
@@ -7,44 +7,28 @@ using System.Threading.Tasks;
 
 namespace GraphExperiment {
     public enum SignalType : int {
-        Sine = 0,
-        Square = 1,
-        Noise = 2,
+        Sine,
+        Square,
+        Noise,
+        Sawtooth,
+        Triangle,
     }
 
     [AudioNode]
     public class Generator : AudioNode {
-
         public double Frequency;
         [AudioNodeParameter]
         public SignalType SignalType;
-        private Random Rnd;
+        private Oscillator Osc;
 
         public Generator(short id) : base(id) {
-            Rnd = new Random();
+            Osc = new Oscillator();
         }
 
         protected override Packet Update(Packet packet) {
-            Sample sample = null;
-            switch (SignalType) {
-                case SignalType.Sine: sample = new Sample(Sine(packet.Sample.L, packet.Tick)); break;
-                case SignalType.Square: sample = new Sample(Square(packet.Sample.L, packet.Tick)); break;
-                case SignalType.Noise: sample = new Sample(Noise()); break;
-            }
-            packet.Sample = sample;
+            Osc.SetFrequency(packet.Sample.L);
+            packet.Sample = new Sample(Osc.Next(SignalType));
             return packet;
-        }
-
-        private double Sine(double frequency, int t) {
-            return Math.Sin(frequency * Math.PI * 2 * t / 44100);
-        }
-
-        private double Square(double frequency, int t) {
-            return Sine(frequency, t) > 0 ? 1 : -1;
-        }
-
-        private double Noise() {
-            return Rnd.NextDouble() * 2 - 1;
         }
     }
 }
