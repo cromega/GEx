01c76922f1b3631d5451a8449cfbba9d5aefc59b
Bence Monus <crome@moronia.hu>
2018-08-29 16:37:05 +0100

    add vibratio filter

---
+++ GraphExperiment/AudioNodes/Vibrator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphExperiment;
+
+namespace GraphExperiment {
+    [AudioNode]
+    public class Vibrator : AudioNode {
+        [AudioNodeParameter]
+        public SignalType Signal;
+
+        [AudioNodeParameter]
+        public double Frequency;
+
+        [AudioNodeParameter]
+        public double Amount;
+
+        private Oscillator Osc;
+
+        public Vibrator(short id) : base(id) {
+            Osc = new Oscillator();
+        }
+
+        protected override Packet Update(Packet packet) {
+            Osc.SetFrequency(Frequency);
+            packet.Sample *= Osc.Next(Signal);
+            return packet;
+        }
+    }
+}
