1f883f7c9a3939021e6544d6062a668ad8a66cf9
Bence Monus <crome@moronia.hu>
2018-09-09 20:15:49 +0100

    add completely broken filter

--- GraphExperiment/AudioNodes/PassFilter.cs
+++ GraphExperiment/AudioNodes/PassFilter.cs
@@ -4,28 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GraphExperiment{
+namespace GraphExperiment {
+    public enum FilterType {
+        LoPass,
+        HiPass,
+    }
+
     [AudioNode(Direction = AudioNodeDirection.InputOutput)]
     public class PassFilter : AudioNode {
+        [AudioNodeParameter]
+        public FilterType Filter;
+
+        [AudioNodeParameter]
         public int Cutoff;
 
-        private double DT;
-        private double RC;
-        private double alpha;
+        private const double DT = 1d / 44100;
 
         public PassFilter(short id) : base(id) {
-            Cutoff = 500;
-
-            DT = 1d / 44100;
-            RC = 1.0 / Cutoff * 2 * Math.PI;
-            alpha = DT / (RC + DT);
+            Cutoff = 440;
         }
 
         protected override Packet Update(Packet packet) {
-            var lastSample = Get<double>("LastSample", packet.Sample.L * alpha);
-            //var sample = lastSample + (alpha * (packet.Sample.L - lastSample));
-            //var sample = alpha * lastSample  + alpha * ()
-            Save("LastSample", sample);
+            var RC = 1.0 / Cutoff * 2 * Math.PI;
+            var alpha = DT / (RC + DT);
+
+            double previousFiltered = Get<double>("PreviousFiltered", 0d);
+            double previousRaw = Get<double>("PreviousRaw", 0d);
+
+            double sample = 0;
+
+            switch (Filter) {
+                case FilterType.LoPass:
+                    sample = alpha * packet.Sample.L + (1 - alpha) * previousFiltered;
+                    break;
+                case FilterType.HiPass:
+                    sample = alpha * (previousFiltered) + alpha * (packet.Sample.L - previousRaw);
+                    break;
+
+            }
+            Save("PreviousFiltered", sample);
+            Save("PreviousRaw", packet.Sample.L);
+
             packet.Sample = new Sample(sample);
             return packet;
         }
